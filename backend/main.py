605a07c9dfc6aeaf4bb0cbf8d5f8149d20d35199
Chika Ngwu <chikangwu95@gmail.com>
2021-09-07 13:51:44 +0200

    added basic setup with docker

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup() -> None:
+    """Event handler running at startup."""
+    pass
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    """Event handler running at shutdown."""
+    pass
