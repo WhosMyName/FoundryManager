0b83cb69a0e2ba41eac8bc04ae76599b39ff306a
Chika Ngwu <chikangwu95@gmail.com>
2021-09-07 14:01:59 +0200

    added wip base_controller

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.on_event("startup")
 async def startup() -> None:
     """Event handler running at startup."""
     pass
+
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
